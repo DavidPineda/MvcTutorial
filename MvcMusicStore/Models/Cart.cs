2583b06a18f0e58be6a60caf57ab768930d05df8
David Pineda <davidpineda1003@gmail.com>
2015-07-15 23:13:44 -0500

    Se agrega el carrito de compras

--- MvcMusicStore/Models/Cart.cs
+++ MvcMusicStore/Models/Cart.cs
@@ -9,7 +9,7 @@ namespace MvcMusicStore.Models
     public class Cart
     {
         [Key]
-        public int RecordID { get; set; }
+        public int RecordId { get; set; }
         public string CartId { get; set; }
         public int AlbumId { get; set; }
         public int Count { get; set; }
